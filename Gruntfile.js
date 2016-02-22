b37b68feaf51eca55e2fe1e8d0af3b8b0aadb694
Raphael Faria <raphaelfmf@gmail.com>
2016-02-21 21:20:57 -0300

    Adds gh-pages deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,13 +35,21 @@ module.exports = function (grunt) {
         tasks: ['sass'],
       },
     },
+    'gh-pages': {
+      options: {
+        base: 'build',
+      },
+      src: ['**'],
+    },
   });
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   grunt.registerTask('default', ['watch']);
   grunt.registerTask('build', ['copy', 'sass', 'browserify']);
+  grunt.registerTask('deploy', ['build', 'gh-pages']);
 };
