d0d0219e11ab9d7dca5722d97b4d556669c44821
Raphael Faria <raphaelfmf@gmail.com>
2016-02-21 21:04:34 -0300

    Fixes firefox timeout issue

--- javascript/helpers/location.js
+++ javascript/helpers/location.js
@@ -1,13 +1,34 @@
 module.exports = {
   getLocation() {
-    return new Promise(function (resolve, reject) {
+    return new Promise((resolve, reject) => {
+      const isFirefox = /firefox/i.test(navigator.userAgent);
+      let timedOut = false;
+
       if (!navigator.geolocation) {
         reject();
       }
 
+      if (isFirefox) {
+        setTimeout(() => {
+          timedOut = true;
+          reject();
+        }, 10000);
+      }
+
       navigator
         .geolocation
-        .getCurrentPosition((position) => resolve(position.coords), reject);
+        .getCurrentPosition(
+          (position) => {
+            if (timedOut) return false;
+
+            return resolve(position.coords);
+          },
+          () => {
+            if (timedOut) return false;
+
+            return reject();
+          },
+          { timeout: 10000 });
     });
   },
 };
--- server.js
+++ server.js
@@ -4,3 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/build'));
 
 app.listen(8000);
+
+console.log('Listening on port 8000');
